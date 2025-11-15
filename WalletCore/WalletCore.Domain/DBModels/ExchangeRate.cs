f2ae6447b5f6bd7ad25ba1274d42930d8c47786d
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-11-16 01:37:31 +0200

    Implement New Project Infrastructure and add DbContext with EFCore

---
+++ WalletCore/WalletCore.Domain/DBModels/ExchangeRate.cs
@@ -0,0 +1,11 @@
+﻿namespace WalletCore.Domain.DBModels
+{
+    public class ExchangeRate
+    {
+        public Guid Id { get; set; }
+        public DateOnly Date { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
