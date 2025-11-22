a960d98fb7abb4548d28b2a5fd66fd3debcd21ea
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-11-23 00:36:54 +0200

    Add enum anotations for WalletStrategyOperation for the needs of Deserialization

--- WalletCore/WalletCore.Domain/Models/AdjustBalance/WalletStrategyOperation.cs
+++ WalletCore/WalletCore.Domain/Models/AdjustBalance/WalletStrategyOperation.cs
@@ -1,9 +1,16 @@
-﻿namespace WalletCore.Domain.Models.AdjustBalance
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace WalletCore.Domain.Models.AdjustBalance
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum WalletStrategyOperation
     {
+        [EnumMember(Value = "subtractFunds")]
         SubtractFunds,
+        [EnumMember(Value = "addFunds")]
         AddFunds,
+        [EnumMember(Value = "forceSubtractFunds")]
         ForceSubtractFunds
     }
 }
