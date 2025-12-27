c21bc18569c022e7d68ad16ccac7f8381f13206e
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-12-28 01:17:11 +0200

    Create Logging package

---
+++ WalletCore.Logging/WalletCore.Logging/GlobalLog.cs
@@ -0,0 +1,17 @@
+﻿namespace WalletCore.Logging
+{
+    public class GlobalLog
+    {
+        public string TransactionId { get; init; }
+        public string RequestUUID { get; init; }
+        public string Direction { get; init; }
+        public string Service { get; init; }
+        public object? Payload { get; init; }
+        public DateTime Timestamp { get; init; }
+        public string Level { get; init; }
+        public string Endpoint { get; init; }
+        public string Environment { get; init; }
+        public string Message { get; init; }
+        public string Exception { get; init; }
+    }
+}
